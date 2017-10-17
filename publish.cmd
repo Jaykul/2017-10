f649ff9a49734a2c3074b14c640fb1d0d8020c5c
Joel Bennett <Jaykul@HuddledMasses.org>
2017-10-17 15:03:14 -0400

    Initial Commit - Generated Code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=PowerShellKB-Web-Deploy.pubxml -p:Password=9A6poxFgSjAdwjfhq6Mmsucldu7la5sno5XuzcHnlfcYbw9PikTwe8jKnNoy
+
