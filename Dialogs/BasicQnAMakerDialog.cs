85a1a8bd4ab35992257f651656d65a091f5c6d21
Joel Bennett <Jaykul@HuddledMasses.org>
2017-10-23 23:23:11 -0400

    Tweak defaults

--- Dialogs/BasicQnAMakerDialog.cs
+++ Dialogs/BasicQnAMakerDialog.cs
@@ -10,7 +10,8 @@ namespace Microsoft.Bot.Sample.QnABot
     public class BasicQnAMakerDialog : QnAMakerDialog
     {
         // Go to https://qnamaker.ai and feed data, train & publish your QnA Knowledgebase.
-        public BasicQnAMakerDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnASubscriptionKey"], ConfigurationManager.AppSettings["QnAKnowledgebaseId"])))  // If you're running this bot locally, make sure you have these appSettings in youe web.config
+        // If you're running this bot locally, make sure you have these appSettings in your web.config
+        public BasicQnAMakerDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnASubscriptionKey"], ConfigurationManager.AppSettings["QnAKnowledgebaseId"], defaultMessage: "", qnaThreshold: 0.5, top: 1)))
         {
         }
     }
